c75cd0a5ff7301766eee4a32f79538191ee246fc
Andrew Broekman <andrewbroekman@gmail.com>
2016-03-19 22:32:57 +0200

    Adds an initial REST mapping to get a PublicationTypeResource, however is is incomplete and will be completed as soon as teams deliver interfaces

--- src/main/java/com/codinginfinity/research/rest/publication/PublicationTypeResource.java
+++ src/main/java/com/codinginfinity/research/rest/publication/PublicationTypeResource.java
@@ -1,10 +1,9 @@
 package com.codinginfinity.research.rest.publication;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -13,6 +12,10 @@ import javax.ws.rs.core.MediaType;
 @Path("publicationTypes")
 public class PublicationTypeResource {
 
+    /*
+     * As teams have not yet defined interfaces, code will not compile, as we are awaiting on teams to complete there
+     * interfaces.
+     */
     @Inject
     private PublicationTypes publicationTypes;
 
@@ -22,6 +25,14 @@ public class PublicationTypeResource {
         // ToDo: Get result and check whether status was successfull
         // ToDo: Confirm if lecturers whether we are allowed to return response objects
         publicationTypes.addPublicationType(new AddPublicationTypeRequest(publicationType));
+    }
+
+    @GET
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public GetPublicationTypeResponse get(@PathParam("id") String id) {
+        publicationTypes.getPublicationTypes(new GetPublicationTypeRequest(id));
+        throw new NotImplementedException();
     }
 
     @POST
