06355198e947405e50df4cb408108720f0b971be
Andrew Broekman <andrewbroekman@gmail.com>
2016-04-11 22:04:57 +0200

    Setup unit test Java class files

---
+++ src/test/java/com/codinginfinity/research/people/PeopleTest.java
@@ -0,0 +1,14 @@
+package com.codinginfinity.research.people;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by andrew on 2016/04/11.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {PeopleTest.class})
+public class PeopleTest {
+
+}
