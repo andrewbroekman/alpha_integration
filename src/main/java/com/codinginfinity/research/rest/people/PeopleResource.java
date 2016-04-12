168729dd451134262e4f44dd4bd29a88f9f563bc
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-04-12 20:03:11 +0200

    Rest Wrapping editPersonDetails

---
+++ src/main/java/com/codinginfinity/research/rest/people/PeopleResource.java
@@ -0,0 +1,28 @@
+package com.codinginfinity.research.rest.people;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by Reinhardt on 2016/04/05.
+ */
+ 
+
+@Path("people")
+public class PeopleResource {
+
+	@Inject
+	private Person person;
+	
+	@Inject
+	private Group group;
+	
+	@Post
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void editPersonDetails(Person person){
+		
+	}
+	
+	
+}
