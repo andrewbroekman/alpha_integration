cb8a926a5c3d460f9b0875ca8bb06b6a17a6917d
Reinhardt Cromhout <reinhardt.cromhout@gmail.com>
2016-04-14 18:11:12 +0200

    calcAccreditationPointsForGroup Unit test added

--- src/test/java/com/codinginfinity/research/publication/PublicationTest.java
+++ src/test/java/com/codinginfinity/research/publication/PublicationTest.java
@@ -1,7 +1,10 @@
 package com.codinginfinity.research.publication;
 
+import com.codinginfinity.research.publication.exception.GroupDoesNotExist;
 import com.codinginfinity.research.publication.exception.PersonDoesNotExist;
+import com.codinginfinity.research.publication.request.CalcAccreditationPointsForGroupRequest;
 import com.codinginfinity.research.publication.request.CalcAccreditationPointsForPersonRequest;
+import com.codinginfinity.research.publication.response.CalcAccreditationPointsForGroupResponse;
 import com.codinginfinity.research.publication.response.CalcAccreditationPointsForPersonResponse;
 import com.codinginfinity.research.services.RequestNotValidException;
 import org.junit.Ignore;
@@ -26,6 +29,7 @@ public class PublicationTest {
     @Inject
     IPublication publicationMock;
 
+    //calcAccreditationPointsForPerson
     @Test(expected = RequestNotValidException.class)
     public void calcAccreditationPointsForInvalidPerson() throws PersonDoesNotExist, RequestNotValidException {
         CalcAccreditationPointsForPersonRequest req = new CalcAccreditationPointsForPersonRequest(-1, new Period(createOlderDate(), createNewerDate()));
@@ -43,6 +47,26 @@ public class PublicationTest {
         CalcAccreditationPointsForPersonRequest req = new CalcAccreditationPointsForPersonRequest(111, new Period(createOlderDate(), createNewerDate()));
         CalcAccreditationPointsForPersonResponse resp = publicationMock.calcAccreditationPointsForPerson(req);
         assert resp.getPoints() == 10;
+    }
+
+    //calcAccreditationPointsForGroup
+    @Test(expected = RequestNotValidException.class)
+    public void calcAccreditationPointsForInvalidGroup() throws RequestNotValidException, GroupDoesNotExist {
+        CalcAccreditationPointsForGroupRequest req = new CalcAccreditationPointsForGroupRequest(-1, new Period(createOlderDate(), createNewerDate()));
+        publicationMock.calcAccreditationPointsForGroup(req);
+    }
+
+    @Test(expected = GroupDoesNotExist.class)
+    public void calcAccreditationPointsForNoExistentGroup() throws RequestNotValidException, GroupDoesNotExist {
+        CalcAccreditationPointsForGroupRequest req = new CalcAccreditationPointsForGroupRequest(6, new Period(createOlderDate(), createNewerDate()));
+        publicationMock.calcAccreditationPointsForGroup(req);
+    }
+
+    @Test
+    public void calcAccreditationPointsForNormalGroup() throws RequestNotValidException, GroupDoesNotExist {
+        CalcAccreditationPointsForGroupRequest req = new CalcAccreditationPointsForGroupRequest(222, new Period(createOlderDate(), createNewerDate()));
+        CalcAccreditationPointsForGroupResponse resp = publicationMock.calcAccreditationPointsForGroup(req);
+        assert resp.getPoints() == 26;
     }
 
     public Date createOlderDate(){
